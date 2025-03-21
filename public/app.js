3c1c9b3f46ad3f9a0e810837e1717577283aa826
Vaibhav Yadav <vaibhavyaduvanshi2503@gmail.com>
2025-03-21 09:48:59 +0530

    improve dark theme

--- public/app.js
+++ public/app.js
@@ -1,22 +1,22 @@
-let count = 1;
+let count = 0;
 let bd = document.querySelector("body");
 let theme = document.querySelector(".theme");
 let heading = document.querySelector("h1");
-let posts = document.querySelector(".post");
-let user = document.querySelector(".user");
+let posts = document.querySelectorAll(".post");
+let users = document.querySelectorAll(".user");
 
 // Function to apply the theme
 function applyTheme(themeMode) {
     if (themeMode === "light") {
         bd.style.backgroundColor = `#FFEDFA`;
         heading.style.color = `#EC7FA9`;
-        posts.style.backgroundColor = `#FFB8E0`;
-        user.style.backgroundColor = `#BE5985`;
+        posts.forEach(post => post.style.backgroundColor= `#FFB8E0`);
+        users.forEach(user => user.style.backgroundColor= `#BE5985`);
     } else {
         bd.style.backgroundColor = '#D1F8EF';
         heading.style.color = `#3674B5`;
-        posts.style.backgroundColor = `#A1E3F9`;
-        user.style.backgroundColor = `#578FCA`;
+        posts.forEach(post => post.style.backgroundColor= `#A1E3F9`);
+        users.forEach(user => user.style.backgroundColor= `#578FCA`);
     }
 }
 
